8724852d70eaaa3cb7e1b149961a18db1b6abd2f
renardobleu <80927362+renardobleu@users.noreply.github.com>
2021-03-20 17:08:22 +0100

    Updates

---
+++ precache-manifest.0d750dcfb39d55fe1297bdbfa6f6648f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e502e601f73e7bc8fc3c68c4bff336",
+    "url": "/sunrise-battle-sim-client/index.html"
+  },
+  {
+    "revision": "f80b245d203b694f7efb",
+    "url": "/sunrise-battle-sim-client/static/css/main.fe4cbec9.chunk.css"
+  },
+  {
+    "revision": "9c0d4667c316c0f6fa12",
+    "url": "/sunrise-battle-sim-client/static/js/2.907ddc13.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/sunrise-battle-sim-client/static/js/2.907ddc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80b245d203b694f7efb",
+    "url": "/sunrise-battle-sim-client/static/js/main.8659e9ba.chunk.js"
+  },
+  {
+    "revision": "6fbaf75004dfaf66d711",
+    "url": "/sunrise-battle-sim-client/static/js/runtime-main.afab3532.js"
+  },
+  {
+    "revision": "65780863e7302fe257df982341d6a1cb",
+    "url": "/sunrise-battle-sim-client/static/media/pkmnRGB.65780863.ttf"
+  }
+]);
